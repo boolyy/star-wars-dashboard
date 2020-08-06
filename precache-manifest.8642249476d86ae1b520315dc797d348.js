b9a09ae134c78c626e6e4f9d3c81c4e85ba780b6
Abdullah Bashir <abdullahbashir7@gmail.com>
2020-08-06 19:04:59 -0400

    Updates

---
+++ precache-manifest.8642249476d86ae1b520315dc797d348.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22eca119b37d23d311576c0ca2543b57",
+    "url": "/star-wars-dashboard/index.html"
+  },
+  {
+    "revision": "903ce07189970ec41dc8",
+    "url": "/star-wars-dashboard/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e54af80c5fae116173db",
+    "url": "/star-wars-dashboard/static/css/main.0d2c7894.chunk.css"
+  },
+  {
+    "revision": "903ce07189970ec41dc8",
+    "url": "/star-wars-dashboard/static/js/2.a8d399bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-wars-dashboard/static/js/2.a8d399bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54af80c5fae116173db",
+    "url": "/star-wars-dashboard/static/js/main.eceb71e4.chunk.js"
+  },
+  {
+    "revision": "54c839711179c92cbb1f",
+    "url": "/star-wars-dashboard/static/js/runtime-main.96934272.js"
+  },
+  {
+    "revision": "c53ebe467e7a1cdd3a8488d365e0d355",
+    "url": "/star-wars-dashboard/static/media/star-wars-backgrounds-25.c53ebe46.jpg"
+  }
+]);
